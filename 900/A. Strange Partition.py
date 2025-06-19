50818f8290ad23411913f69225e0024af581210b
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-06-19 19:58:27 +0600

    updated codes

--- 900/A. Strange Partition.py
+++ 900/A. Strange Partition.py
@@ -1,16 +1,15 @@
 import math
 
-t=int(input())
+t = int(input())
 for _ in range(t):
-    n,x=max(int,input().split())
-    a=list(map(int,input().split()))
+    n, x = map(int, input().split())
+    a = list(map(int, input().split()))
 
-    max_sum=0
+    max_sum = 0
     for i in a:
-        max_sum +=math.ceil(i/x)
+        max_sum += math.ceil(i / x)
     
-    total_sum=sum(a)
-    min_sum=math.ceil(total_sum/x)
+    total_sum = sum(a)
+    min_sum = math.ceil(total_sum / x)
 
-    print(min_sum,max_sum)
-    
+    print(min_sum, max_sum)
