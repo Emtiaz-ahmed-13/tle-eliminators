0150083e976c30a34343e2baf76f3080c059c120
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-06-27 21:50:07 +0600

    updated codes

---
+++ 900/B. Not Dividing.py
@@ -0,0 +1,14 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int, input().split()))
+
+    for i in range(n):
+        if a[i] ==1:
+            a[i]+=1
+
+    for i in range(n-1):
+        if a[i+1]%a[i]==0:
+            a[i+1]+=1
+
+    print(' '.join(map(str, a)))
