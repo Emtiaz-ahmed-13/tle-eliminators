c44dc64d3caffca13b77ce7aeaefaba3c17c628a
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2025-06-29 21:00:08 +0600

    updated codes

---
+++ 900/A. AvtoBus.py
@@ -0,0 +1,11 @@
+import math
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    if n < 4 or n % 2 == 1:
+        print(-1)
+    else:
+        min_buses = math.ceil(n / 6)
+        max_buses = n // 4
+        print(min_buses, max_buses)
