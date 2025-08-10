382260e727d518d8887204728f01add0c074fa57
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2025-08-11 00:50:31 +0600

    update codes

--- 1100/B. Erase First or Second Letter/B.Erase.cpp
+++ 1100/B. Erase First or Second Letter/B.Erase.cpp
@@ -1,8 +1,28 @@
 #include<bits/stdc++.h>
-using namesapce std;
+using namespace std;
 
 int main(){
-	
-	
-}
+    int t=1;
+    cin>>t;
+    while(t--){
+        int n; cin>>n;
+        string s; cin>>s;
+        map<char,int>freq;
+        int count=0;
+        vector<int>distinct(n,0);
+        for(int i=0;i<n;i++){
+            freq[s[i]]++;
+            if (freq[s[i]]==1){
+                count++;
+            }
+            distinct[i]=count;
 
+        }
+        long long ans=0;
+        for(int i=0;i<n;i++){
+            ans+=distinct[i];
+        }
+        cout<<ans<<endl;
+    }
+
+}
