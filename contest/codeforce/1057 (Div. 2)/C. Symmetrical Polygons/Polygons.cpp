829f917aa307d0d24e0c71f76372c7829bb3b102
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2025-10-14 23:44:02 +0600

    feat: done

--- contest/codeforce/1057 (Div. 2)/C. Symmetrical Polygons/Polygons.cpp
+++ contest/codeforce/1057 (Div. 2)/C. Symmetrical Polygons/Polygons.cpp
@@ -6,24 +6,58 @@
 using namespace std;
 
 typedef long long ll;
-typedef long double ld;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef map<int, int> mii;
-typedef map<ll, ll> mll;
 
 #define FastIO \
     ios_base::sync_with_stdio(false); \
     cin.tie(nullptr); \
     cout.tie(nullptr);
 
-const int md = (int)1e9 + 7;
-
 void TC()
 {
-    // Your dish here
+   
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; ++i) cin >> a[i];
+
+    // frequency map
+    unordered_map<ll, ll> fr;
+    for (ll x : a) fr[x]++;
+
+    vector<ll> v;      
+    ll ans = 0;        
+    ll cnt = 0;        
+
+    for (auto &p : fr) {
+        ll val = p.first;
+        ll freq = p.second;
+        if (freq & 1) v.push_back(val);
+        ans += freq / 2;
+        cnt += (freq / 2) * 2;
+    }
+
+    if (ans == 0) { 
+        cout << 0 << '\n';
+        return;
+    }
+
+    sort(v.begin(), v.end());
+
+    ll finans = 0;
+    for (size_t i = 0; i < v.size(); ++i) {
+        if (v[i] < ans) {
+            finans = max(finans, ans + v[i]);
+        }
+    }
+    for (size_t i = 1; i < v.size(); ++i) {
+        if (v[i] - v[i - 1] < ans) {
+            
+            ll cand = max(ans + v[i], ans + v[i - 1]);
+            finans = max(finans, cand);
+        }
+    }
+
+    cout << finans << '\n';
 }
 
 int main()
@@ -31,9 +65,6 @@ int main()
     FastIO;
     int t = 1;
     cin >> t;
-    for (int i = 1; i <= t; i++)
-    {
-        TC();
-    }
+    while (t--) TC();
     return 0;
 }
